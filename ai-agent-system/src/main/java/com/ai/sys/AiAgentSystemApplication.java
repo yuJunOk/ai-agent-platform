ecd262c77c887fc01e446c52ab5a9e35e5bea2b6
pengYuJun <1375841038@qq.com>
2025-07-19 16:12:57 +0800

    引入 ollama ai 模型调用

--- ai-agent-system/src/main/java/com/ai/sys/AiAgentSystemApplication.java
+++ ai-agent-system/src/main/java/com/ai/sys/AiAgentSystemApplication.java
@@ -3,6 +3,9 @@ package com.ai.sys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author pengYuJun
+ */
 @SpringBootApplication
 public class AiAgentSystemApplication {
 
---
+++ ai-agent-system/src/main/java/com/ai/sys/external/OllamaAiInvoke.java
@@ -0,0 +1,27 @@
+package com.ai.sys.external;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * SpringAI 调用 ollama大模型
+ * @author pengYuJun
+ */
+@Component
+public class OllamaAiInvoke implements CommandLineRunner {
+
+    @Resource
+    private ChatModel ollamaChatModel;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = ollamaChatModel.call(new Prompt("你好，我是你爹"))
+                .getResult()
+                .getOutput();
+        System.out.println(output.getText());
+    }
+}
